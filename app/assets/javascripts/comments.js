69160b43254da1eb39595a384c1121ee70967ad6
Artur Rozhanskyi <artur.rozhanskyi@nixsolutions.com>
2019-11-01 13:00:25 +0200

    Add "Edit comment" for Question

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,8 +1,8 @@
 $(document).on('turbolinks:load', function() {
-  $(document).on('click', '.add_comment', function (e) {
+  $(document).on('click', '.add_comment, .edit_comment_button', function (e) {
     e.preventDefault();
     $(this).toggle();
-    $(this).closest('.question_comments').find('#new_comment_question').toggle();
+    $(this).parent().next().toggle();
   });
 
   $(document).on('ajax:success', '#new_comment_question', function (e) {
@@ -15,7 +15,33 @@ $(document).on('turbolinks:load', function() {
       $(this).find('.question-comment-errors').text(e.detail[0]);
     });
 
+  $(document).on('ajax:success', '.edit_comment', function (e) {
+    comment = $.parseJSON(e.detail[2].responseText);
+    prev_element = $(this).closest('.comment').prev();
+    $(this).closest('.comment').remove();
+    prev_element.after(comment_block(comment));
+    if(prev_element.length === 0){
+      $('.comments').prepend(comment_block(comment));
+    }
+    else {
+      $(prev_elem).after(comment_block(comment));
+    }
+  })
+    .on('ajax:error', '.edit_comment', function(e){
+      $(this).find('.question-comment-errors').text(e.detail[0]);
+    });
+
   function comment_block(comment) {
-    return $('<div>').addClass('comment').append($('<p>').text(comment.body));
+    return $('<div>').addClass('comment')
+      .append($('<p>' + comment.body + '</p>'))
+      .append(comment_form_block(comment));
+  }
+  
+  function comment_form_block(comment) {
+    block = comment_form.content.cloneNode(true);
+    $(block).find('.edit_comment').attr({ 'id': 'edit_comment_' + comment.id,
+                    'action': '/comments/' + comment.id + '.json'});
+    $(block).find('#question_comment_body').val(comment.body);
+    return block;
   }
 });
---
+++ spec/features/user_edit_comment_spec.rb
@@ -0,0 +1,63 @@
+RSpec.describe 'UserEditComments', type: :feature do
+  let(:user) { create(:user) }
+  let!(:question) { create(:question, :with_comment, user: user) }
+  let(:comment) { question.comments.first }
+  let(:new_comment_body) { Faker::Lorem.sentence }
+
+  describe 'Registered user edit comment', js: true do
+    context 'when comment belong to user' do
+      before do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      context 'with valid attribute' do
+        it 'has edit comment link' do
+          within '.comment' do
+            expect(page).to have_button 'Edit'
+          end
+        end
+
+        it 'has update comment' do
+          edit_subject(new_comment_body, 'comment')
+          find '.comments'
+          expect(comment.body).to eq new_comment_body
+        end
+
+        it 'has not old comment body' do
+          old_body = comment.body
+          edit_subject(new_comment_body, 'comment')
+          find '.comments'
+          expect(page).not_to have_content old_body
+        end
+
+        it 'has new comment body' do
+          edit_subject(new_comment_body, 'comment')
+          find '.comments'
+          expect(page).to have_content new_comment_body
+        end
+      end
+
+      context 'with invalid attribute' do
+        let(:invalid_comment) { attributes_for(:invalid_comment) }
+
+        it 'has not updated comment' do
+          old_body = comment.body
+          edit_subject(invalid_comment, 'comment')
+          find '.comments'
+          expect(comment.body).to eq old_body
+        end
+      end
+    end
+
+    context 'when comment not belong to user' do
+      it 'has not comment link' do
+        sign_in(create(:user))
+        visit question_path(question)
+        within '.comment' do
+          expect(page).not_to have_button 'Edit'
+        end
+      end
+    end
+  end
+end
