a00c99b43d170c58d7261beed09b040384099739
Artur Rozhanskyi <artur.rozhanskyi@nixsolutions.com>
2020-04-24 10:17:04 +0300

    Add CORS

---
+++ config/initializers/cors.rb
@@ -0,0 +1,10 @@
+ORIGINS = [ENV.fetch('CLIENT_ORIGIN') { '' }].freeze
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins(*ORIGINS)
+    resource '*',
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
